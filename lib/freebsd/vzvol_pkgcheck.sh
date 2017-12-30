83218e0cfcff4dd244e6645a009bcc19c799946c
Rainbow <rainbow@hacker.horse>
2017-12-30 07:46:09 -0800

    Convert pkg info to pkg -N info to prevent bootstrapping pkg on a clean system.

--- lib/freebsd/vzvol_pkgcheck.sh
+++ lib/freebsd/vzvol_pkgcheck.sh
@@ -4,13 +4,13 @@ vzvol_pkg_check_freebsd(){
 		echo "Warning. You have selected an FS type supplied by a port. Now checking to see if the port is installed."
 		echo "Please note that unsupported FSes may exhibit unexpected behavior!"
 		if [ "${FSTYPE}" = "ext2" -o "${FSTYPE}" = "ext3" -o "${FSTYPE}" = "ext4" ]; then
-			pkg info | grep -q e2fsprogs
+			pkg -N info | grep -q e2fsprogs
 			if [ ! $? = 0 ]; then
 				echo "Error! You need to install sysutils/e2fsprogs first!"
 				return 1
 			fi
 		elif [ "${FSTYPE}" = "xfs" ]; then
-			pkg info | grep -q xfsprogs
+			pkg -N info | grep -q xfsprogs
 			if [ ! $? = 0 ]; then
 				echo "Error! You need to install sysutils/xfsprogs first!"
 				return 1
