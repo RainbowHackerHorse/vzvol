9e98fa5c40774a8f42cea01415efd395ad161c70
Rainbow <rainbow@hacker.horse>
2018-01-11 12:06:59 -0800

    Check for --type and fail if not selected for Create

--- lib/shared/zvol_check.sh
+++ lib/shared/zvol_check.sh
@@ -5,6 +5,13 @@ zvol_check() {
 		echo "Please provide a zvol name. See --help for more information."
 		return 1
 	fi
+	if [ "${VOLTYPE}" = 'NULL' ]; then
+		echo "Error. Type not selected. See --help for more information."
+		echo "You need to select your type with -t or --type."
+		echo "Available types are raw and virtualbox"
+		echo "Example: vzvol -v MyVM --type virtualbox"
+		return 1
+	fi
 	( zfs list -t volume 2>/dev/null | awk '{print $1}' | grep -v "NAME" | grep -q "${ZROOT}/${VOLNAME}" )
 	if [ $? = 0 ]; then
 		NO_CREATE=YES
