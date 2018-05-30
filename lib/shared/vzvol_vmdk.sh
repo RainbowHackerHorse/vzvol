8f4db324a08ead301f87d9b5580d93979b554a03
Rainbow <rainbow@sanitylinux.org>
2018-05-30 14:43:40 -0500

    Change VMDK creation to occur in correct folder

--- lib/shared/vzvol_vmdk.sh
+++ lib/shared/vzvol_vmdk.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 vmdk_create() {
+	ZUSERHOME=$( awk -F ":" '/${ZUSER}/{print $6}' < /etc/passwd )
 	errorfunc='vmdk_create'
-	if [ ! -d "${HOME}"/VBoxdisks/ ]; then
-		mkdir -p "${HOME}"/VBoxdisks/
+	if [ ! -d "${ZUSERHOME}"/VBoxdisks/ ]; then
+		mkdir -p "${ZUSERHOME}"/VBoxdisks/
 	fi
-	if [ ! -e "${HOME}"/VBoxdisks/"${VOLNAME}".vmdk ]; then
-		echo "Creating ${HOME}/VBoxdisks/${VOLNAME}.vmdk"
+	if [ ! -e "${ZUSERHOME}"/VBoxdisks/"${VOLNAME}".vmdk ]; then
+		echo "Creating ${ZUSERHOME}/VBoxdisks/${VOLNAME}.vmdk"
 		sleep 3
 		VBoxManage internalcommands createrawvmdk \
-		-filename "${HOME}"/VBoxdisks/"${VOLNAME}".vmdk \
+		-filename "${ZUSERHOME}"/VBoxdisks/"${VOLNAME}".vmdk \
 		-rawdisk /dev/zvol/"${ZROOT}/${VOLNAME}"
 	else
-		echo "${HOME}/VBoxdisks/${VOLNAME}.vmdk" already exists.
+		echo "${ZUSERHOME}/VBoxdisks/${VOLNAME}.vmdk" already exists.
 		return 1
 	fi
 }
