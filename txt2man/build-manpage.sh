c4187c1cc3ee7858c3024e2716cd2d9fa01f1372
Rainbow <rainbow@sanitylinux.org>
2018-06-08 00:05:02 -0500

    Add new man dir and add version to manpage

--- txt2man/build-manpage.sh
+++ txt2man/build-manpage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 set -e
-txt2man -t vzvol -s 8 -v "FreeBSD System Manager\\'s Manual" VZVOL\(8\).txt > vzvol.freebsd.man
-txt2man -t vzvol -s 8 -v "System Manager\\'s Manual" VZVOL\(8\).txt > vzvol.linux.man
+txt2man -t vzvol -s 8 -r "vzvol Version 0.7.0" -v "FreeBSD System Manager\\'s Manual" VZVOL\(8\).txt > ../man/freebsd/vzvol.8
+txt2man -t vzvol -s 8 -r "vzvol Version 0.7.0" -v "System Manager\\'s Manual" VZVOL\(8\).txt > ../man/linux/vzvol.8
 exit
